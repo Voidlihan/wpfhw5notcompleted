d4cad51727bc7620a1603feb9ab5e44f1379e7f4
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-11-26 17:11:32 +0600

    Добавьте файлы проекта.

---
+++ WPF-HW5/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace WPF_HW5
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-82L5RVF8;Database=ZKHDb;Trusted_Connection=true;");
+        }
+    }
+}
---
+++ WPF-HW5/User.cs
@@ -0,0 +1,6 @@
+﻿namespace WPF_HW5
+{
+    public class User
+    {
+    }
+}
